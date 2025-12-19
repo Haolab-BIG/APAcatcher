31e297494cb2b3879a4050966cf2dc51122d3d92
CC10969Peng <136300660+CC10969Peng@users.noreply.github.com>
2025-12-19 16:32:29 +0800

    Update model_inference.py

--- script/model/model_inference.py
+++ script/model/model_inference.py
@@ -7,10 +7,26 @@ import numpy as np
 
 
 def one_hot_encode(sequences, max_len=201):
-    encoding = {'A': [1, 0, 0, 0], 'C': [0, 1, 0, 0], 'G': [0, 0, 1, 0], 'T': [0, 0, 0, 1],
-                "N": [0.25, 0.25, 0.25, 0.25],
-                'a': [1, 0, 0, 0], 'c': [0, 1, 0, 0], 'g': [0, 0, 1, 0], 't': [0, 0, 0, 1],
-                "n": [0.25, 0.25, 0.25, 0.25]}
+    encoding = {
+        "A": [1, 0, 0, 0],
+        "C": [0, 1, 0, 0],
+        "G": [0, 0, 1, 0],
+        "T": [0, 0, 0, 1],
+
+        "R": [0.5, 0, 0.5, 0],  # A or G
+        "Y": [0, 0.5, 0, 0.5],  # C or T
+        "S": [0, 0.5, 0.5, 0],  # G or C
+        "W": [0.5, 0, 0, 0.5],  # A or T
+        "K": [0, 0, 0.5, 0.5],  # G or T
+        "M": [0.5, 0.5, 0, 0],  # A or C
+
+        "B": [0, 1 / 3, 1 / 3, 1 / 3],  # C/G/T
+        "D": [1 / 3, 0, 1 / 3, 1 / 3],  # A/G/T
+        "H": [1 / 3, 1 / 3, 0, 1 / 3],  # A/C/T
+        "V": [1 / 3, 1 / 3, 1 / 3, 0],  # A/C/G
+        "N": [0.25, 0.25, 0.25, 0.25]
+    }
+    encoding = {**encoding, **{k.lower(): v for k, v in encoding.items()}}
     encoded_sequences = []
     for sequence in sequences:
         encoded_seq = [encoding[base] for base in sequence]
@@ -18,7 +34,6 @@ def one_hot_encode(sequences, max_len=201):
             encoded_seq += [[0.25, 0.25, 0.25, 0.25]] * (max_len - len(sequence))
         encoded_sequences.append(encoded_seq)
     return np.array(encoded_sequences)
-
 
 def Data_prepare_val(df, max_len=201):
     sequences = list(df["sequence"].values)
@@ -47,19 +62,16 @@ def val_model(model, val_data, device="cpu", threshold=0.5):
 
 
 def filter_sequences_with_model(sequences, model, max_len=201):
-    # 创建 DataFrame
     df = pd.DataFrame(sequences, columns=["chrom", "start", "end", "gene", "strand", "sequence"])
     df["start"] = df["start"].astype(int)
     df["end"] = df["end"].astype(int)
-    val_loader = Data_prepare_val(df)
-    probs, labels = val_model(model, val_loader)
-
-    df["label"] = labels  # 直接添加标签列
-    #df["probs"] = probs
+    val_loader = Data_prepare_val(df,max_len)
+    prob, labels = val_model(model, val_loader)
+    df["label"] = labels  
     df["sequence"] = 1
     filtered_sequences = df[df["label"] == 0][["chrom", "start", "end","sequence" ,"gene", "strand"]]
     filtered_sequences.reset_index(drop=True, inplace=True)
-    #drop_sequences =  df[df["label"] == 1][["chrom", "start", "end","sequence" ,"gene", "strand","probs"]]
-    #drop_sequences.reset_index(drop=True, inplace=True)
     return filtered_sequences
+
+
 
