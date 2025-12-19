8fd6fcf9d69ade62465fefb2d000d113488be817
CC10969Peng <136300660+CC10969Peng@users.noreply.github.com>
2025-12-19 16:07:50 +0800

    Update model.py

--- script/model/model.py
+++ script/model/model.py
@@ -25,3 +25,22 @@ class PAS_CNN(nn.Module):
         x = self.fc2(x)
         return x
 
+class PAS_CNN_A(nn.Module):
+    def __init__(self):
+        super(PAS_CNN_A, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=96, kernel_size=(6, 4))
+        self.pool1 = nn.MaxPool2d(kernel_size=(2, 1))
+        self.lstm = nn.LSTM(input_size=96, hidden_size=64, num_layers=1, batch_first=True, bidirectional=True)
+        self.relu = nn.ReLU()
+        self.fc1 = nn.Linear(18944, 256)
+        self.fc2 = nn.Linear(256, 2)
+
+    def forward(self, x):
+        x = self.relu(self.conv1(x))
+        x = self.pool1(x)
+        x = rearrange(x, 'b c h w -> b h (w c)')
+        x, _ = self.lstm(x)
+        x = x.reshape(x.size(0), -1)
+        x = self.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
