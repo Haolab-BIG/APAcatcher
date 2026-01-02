1a96e35b474c918119b781cc8a9e27ac2d3adcb1
CC10969Peng <136300660+CC10969Peng@users.noreply.github.com>
2026-01-02 15:54:56 +0800

    Add files via upload

---
+++ demo/download.sh
@@ -0,0 +1,8 @@
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR507/007/SRR5076687/SRR5076687_2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR507/007/SRR5076687/SRR5076687_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR507/008/SRR5076688/SRR5076688_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR507/008/SRR5076688/SRR5076688_2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR507/002/SRR5076712/SRR5076712_2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR507/002/SRR5076712/SRR5076712_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR507/003/SRR5076713/SRR5076713_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR507/003/SRR5076713/SRR5076713_2.fastq.gz
